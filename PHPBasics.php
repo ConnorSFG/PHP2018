b5dc6ece4f518947f4a2955f20d69b5ade9a94f0
Connor Nichols <connorouting@yahoo.com>
2018-08-31 17:30:52 -0500

    PHP Basics Upload
    Uploading Assignment

---
+++ PHPBasics.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP Basics</title>
+</head>
+
+<body>
+
+<?php echo "<h1>PHP Elements Assignment</h1>" ?>
+
+<?php $yourname= "Connor N"; ?>
+
+<?php echo "<h2>$yourname</h2>" ?>
+
+<?php $number1 = "2"; ?>
+
+<?php $number2 = "3"; ?>
+
+<?php $total = "$number1 + $number2"; ?>
+
+<?php echo "<p>$number1</p>"; ?>
+
+<?php echo "<p>$number2</p>"; ?>
+
+<?php echo "<p>$total</p>"; ?>
+
+<?php echo ($number1 + $number2); ?>
+
+<br />
+
+<script>
+
+<?php echo "var code = ('PHP', 'HTML', 'Javascript');";?>
+
+</script>
+
+</body>
+</html>
